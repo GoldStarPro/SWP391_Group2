f2309872b76ba87226c657616def7bb1c64c5114
HoangVersion <huyhoangero@gmail.com>
2024-06-18 23:49:04 +0700

    Update database_Add Project Table

--- Database/SQLQuery_HR_Management.sql
+++ Database/SQLQuery_HR_Management.sql
@@ -40,6 +40,16 @@ CREATE TABLE [dbo].[unit](
 );
 GO
 
+CREATE TABLE [dbo].[project](
+	[project_id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	[project_name] NVARCHAR(100) NOT NULL,
+	[start_date] DATETIME NULL,
+	[end_date] DATETIME NULL,
+	[status] NVARCHAR(50) NULL,
+	[notes] NVARCHAR(200) NULL
+);
+GO
+
 CREATE TABLE [dbo].[salary](
 	[salary_id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	[expertise_id] INT,
@@ -86,6 +96,7 @@ CREATE TABLE [dbo].[employee](
 	[social_insurance_id] INT,
 	[salary_id] INT,
 	[unit_id] INT,
+	[project_id] INT,
 	[tax_id] INT,
 	[expertise_id] INT,
 	[email] [nvarchar](100) NULL,
@@ -100,6 +111,7 @@ CREATE TABLE [dbo].[employee](
 	FOREIGN KEY ([social_insurance_id]) REFERENCES [dbo].[social_insurance]([social_insurance_id]),
 	FOREIGN KEY ([salary_id]) REFERENCES [dbo].[salary]([salary_id]),
 	FOREIGN KEY ([unit_id]) REFERENCES [dbo].[unit]([unit_id]),
+	FOREIGN KEY ([project_id]) REFERENCES [dbo].[project]([project_id]),
 	FOREIGN KEY ([tax_id]) REFERENCES [dbo].[personal_income_tax]([tax_id]),
 	FOREIGN KEY ([expertise_id]) REFERENCES [dbo].[expertise]([expertise_id]),
 )
@@ -164,6 +176,20 @@ INSERT [dbo].[unit] ( [unit_name], [notes]) VALUES ( N'Planning - Distribution D
 INSERT [dbo].[unit] ( [unit_name], [notes]) VALUES ( N'Production Management Department', N'No')
 INSERT [dbo].[unit] ( [unit_name], [notes]) VALUES ( N'Bussiness Department', N'Không')
 
+INSERT INTO [dbo].[project] (project_name, start_date, end_date, status, notes)
+VALUES 
+(N'Product Development Project A', '2024-01-01', '2024-12-31', N'In Progress', N'Developing a new product for the domestic market'),
+(N'Production Process Optimization', '2024-02-15', '2024-08-15', N'In Progress', N'Reducing costs and increasing production efficiency'),
+(N'Southern Market Expansion', '2024-03-01', '2024-09-30', N'In Progress', N'Implementing a new business campaign in the southern region'),
+(N'ERP System Improvement Project', '2024-01-15', '2024-06-30', N'Completed', N'Improving and upgrading the current ERP system'),
+(N'European Market Research', '2024-04-01', '2024-10-31', N'In Progress', N'Researching the potential for expanding into the European market'),
+(N'Customer Relationship Management (CRM) Implementation', '2024-05-01', '2024-11-30', N'In Progress', N'Deploying a new CRM system to enhance customer relations'),
+(N'Office Renovation', '2024-06-01', '2024-07-31', N'Planned', N'Renovating the main office to improve working conditions'),
+(N'Supply Chain Efficiency Project', '2024-07-01', '2024-12-31', N'In Progress', N'Optimizing the supply chain to reduce delays and costs'),
+(N'New Marketing Strategy', '2024-08-01', '2024-12-31', N'In Progress', N'Developing and implementing a new marketing strategy for the upcoming year'),
+(N'IT Infrastructure Upgrade', '2024-09-01', '2025-02-28', N'In Progress', N'Upgrading the company’s IT infrastructure to improve performance and security');
+
+
 INSERT [dbo].[qualification] ([qualification_name], [notes]) VALUES (N'C', N'College')
 INSERT [dbo].[qualification] ([qualification_name], [notes]) VALUES (N'Pg', N'Postgraduate')
 INSERT [dbo].[qualification] ([qualification_name], [notes]) VALUES (N'U', N'University')
@@ -178,15 +204,12 @@ INSERT [dbo].[personal_income_tax] ([tax_authority], [salary_id], [amount], [reg
 INSERT [dbo].[personal_income_tax] ([tax_authority], [salary_id], [amount], [registration_date], [notes]) VALUES (N'Cục Thuế Bình Định', 3, 350000, CAST(0x0000A6DA00000000 AS DateTime), N'Không Có')
 INSERT [dbo].[personal_income_tax] ([tax_authority], [salary_id], [amount], [registration_date], [notes]) VALUES (N'Cục Thuế Phú Yên', 4, 550000, CAST(0x0000A6DA00000000 AS DateTime), N'Không Có')
 
-INSERT [dbo].[employee] ([full_name], [date_of_birth], [gender], [id_card_number], [place_of_birth], [address], [phone_number], [qualification_id], [social_insurance_id], [salary_id], [unit_id], [tax_id], [expertise_id], [email], [notes], [ethnicity], [religion], [nationality], [password],[permisson]) 
-VALUES (N'Trần Huy Hoàng', CAST(0x0000884600000000 AS DateTime), N'Male', N'215487745   ', N'Bình Định', N'TP QN', N'0913201503 ', 3, 1, 1, 2, 1, 1, N'huyhoangero@gmail.com', N'Không ', N'Kinh', N'Không', N'Việt Nam', '123456',1)
-INSERT [dbo].[employee] ([full_name], [date_of_birth], [gender], [id_card_number], [place_of_birth], [address], [phone_number], [qualification_id], [social_insurance_id], [salary_id], [unit_id], [tax_id], [expertise_id], [email], [notes], [ethnicity], [religion], [nationality], [password],[permisson]) 
-VALUES (N'Lê Việt Thy', CAST(0x0000A60100000000 AS DateTime), N'Female', N'242523652   ', N'Phú Yên', N'TP Tuy Hòa', N'0822543757 ', 3, 2, 2, 3, 2,2, N'levietthy03@gmail.com', N'Không', N'Kinh', N'Không', N'Việt Nam', '123456',1)
-INSERT [dbo].[employee] ([full_name], [date_of_birth], [gender], [id_card_number], [place_of_birth], [address], [phone_number], [qualification_id], [social_insurance_id], [salary_id], [unit_id], [tax_id], [expertise_id], [email], [notes], [ethnicity], [religion], [nationality], [password],[permisson]) 
-VALUES (N'Phan Phương Sinh', CAST(0x0000806800000000 AS DateTime), N'Male', N'212823367   ', N'Bình Định', N'TP Quy Nhơn', N'0522991730 ', 3, 3, 3, 1, 3, 3, N'sinh123123444@gmail.com', N'Không', N'Kinh', N'Không', N'Việt Nam','123456',2)
-INSERT [dbo].[employee] ([full_name], [date_of_birth], [gender], [id_card_number], [place_of_birth], [address], [phone_number], [qualification_id], [social_insurance_id], [salary_id], [unit_id], [tax_id], [expertise_id], [email], [notes], [ethnicity], [religion], [nationality], [password],[permisson]) 
-VALUES (N'Nguyễn Ngô Chiến', CAST(0x0000806800000000 AS DateTime), N'Male', N'212823367   ', N'Bình Định', N'TP Quy Nhơn', N'0978177739 ', 3, 4, 4, 7, 4, 4, N'chienlag1@gmail.com', N'Không', N'Kinh', N'Không', N'Việt Nam', '123456',3)
-INSERT [dbo].[employee] ([full_name], [date_of_birth], [gender], [id_card_number], [place_of_birth], [address], [phone_number], [qualification_id], [social_insurance_id], [salary_id], [unit_id], [tax_id], [expertise_id], [email], [notes], [ethnicity], [religion], [nationality], [password],[permisson]) 
-VALUES (N'Phan Quốc Đại', CAST(0x0000806800000000 AS DateTime), N'Male', N'216849367   ', N'Bình Định', N'TP Quy Nhơn', N'0976697093 ', 3, 4, 4, 7, 4, 4, N'daiphan245@gmail.com', N'Không', N'Kinh', N'Không', N'Việt Nam', '123456',2)
+INSERT [dbo].[employee] ([full_name], [date_of_birth], [gender], [id_card_number], [place_of_birth], [address], [phone_number], [qualification_id], [social_insurance_id], [salary_id], [unit_id], [project_id], [tax_id], [expertise_id], [email], [notes], [ethnicity], [religion], [nationality], [password],[permisson]) 
+VALUES 
+(N'Trần Huy Hoàng', CAST(0x0000884600000000 AS DateTime), N'Male', N'215487745', N'Bình Định', N'TP QN', N'0913201503 ', 3, 1, 1, 2, 3, 1, 1, N'huyhoangero@gmail.com', N'Không ', N'Kinh', N'Không', N'Việt Nam', '123456',1),
+(N'Lê Việt Thy', CAST(0x0000A60100000000 AS DateTime), N'Female', N'242523652', N'Phú Yên', N'TP Tuy Hòa', N'0822543757 ', 3, 2, 2, 3, 3, 2,2, N'levietthy03@gmail.com', N'Không', N'Kinh', N'Không', N'Việt Nam', '123456',1),
+(N'Phan Phương Sinh', CAST(0x0000806800000000 AS DateTime), N'Male', N'212823367', N'Bình Định', N'TP Quy Nhơn', N'0522991730 ', 3, 3, 3, 1, 2, 3, 3, N'sinh123123444@gmail.com', N'Không', N'Kinh', N'Không', N'Việt Nam','123456',2),
+(N'Nguyễn Ngô Chiến', CAST(0x0000806800000000 AS DateTime), N'Male', N'212823367', N'Bình Định', N'TP Quy Nhơn', N'0978177739 ', 3, 4, 4, 7, 1, 4, 4, N'chienlag1@gmail.com', N'Không', N'Kinh', N'Không', N'Việt Nam', '123456',3),
+(N'Phan Quốc Đại', CAST(0x0000806800000000 AS DateTime), N'Male', N'216849367', N'Bình Định', N'TP Quy Nhơn', N'0976697093 ', 3, 4, 4, 7, 5, 4, 4, N'daiphan245@gmail.com', N'Không', N'Kinh', N'Không', N'Việt Nam', '123456',2);
 
 
