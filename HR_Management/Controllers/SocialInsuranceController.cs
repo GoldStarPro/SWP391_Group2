caa565b4b264700e40e901c8b2c25ae5f48fdca7
HoangVersion <huyhoangero@gmail.com>
2024-06-25 15:26:19 +0700

    show dropdown RegisteredMedicalFacility for edit in SocialInsurance

--- HR_Management/Controllers/SocialInsuranceController.cs
+++ HR_Management/Controllers/SocialInsuranceController.cs
@@ -75,6 +75,7 @@ namespace HR_Management.Controllers
             {
                 return NotFound();
             }
+            ViewData["Social_Insurance_ID"] = new SelectList(_context.SocialInsurances, "Social_Insurance_ID", "Registered_Medical_Facility", socialInsurances.Social_Insurance_ID);
             return View(socialInsurances);
         }
 
@@ -108,6 +109,7 @@ namespace HR_Management.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
+            ViewData["Social_Insurance_ID"] = new SelectList(_context.SocialInsurances, "Social_Insurance_ID", "Social_Insurance_ID", socialInsurances.Social_Insurance_ID);
             return View(socialInsurances);
         }
 
