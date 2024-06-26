af2e106a85f0db5764ecabd4b43ed7c9347dfa03
HoangVersion <huyhoangero@gmail.com>
2024-06-26 15:13:16 +0700

    Done Project Part V1

--- HR_Management/Models/Project.cs
+++ HR_Management/Models/Project.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -12,11 +13,25 @@ namespace HR_Management.Models
             Employees = new HashSet<Employee>();
         }
 
+
+        [Key]
         public int Project_ID { get; set; }
+
+        [Required(ErrorMessage = "Project Name is required.")]
+        [MaxLength(100, ErrorMessage = "Project Name cannot be longer than 100 characters.")]
         public string Project_Name { get; set; }
-        public DateTime? Start_Date { get; set; }
-        public DateTime? End_Date { get; set; }
+
+        [Required(ErrorMessage ="Start Date is required.")]
+        public DateTime Start_Date { get; set; }
+
+        [Required(ErrorMessage = "End Date is required.")]
+        public DateTime End_Date { get; set; }
+
+        [Required(ErrorMessage = "Status is required.")]
+        [MaxLength(50, ErrorMessage = "Status cannot be longer than 50 characters.")]
         public string Status { get; set; }
+
+        [MaxLength(200, ErrorMessage = "Notes cannot be longer than 200 characters.")]
         public string Notes { get; set; }
 
         public virtual ICollection<Employee> Employees { get; set; }
